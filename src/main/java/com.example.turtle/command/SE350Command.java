466d62c7a50650370db8ec93c959fa3ad8169046
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:52:48 -0500

    Draws “SE 350” by sequencing LetterS, LetterE, space, DigitThree, space, Rectangle (0).

--- src/main/java/com.example.turtle/command/SE350Command.java
+++ src/main/java/com.example.turtle/command/SE350Command.java
@@ -1,4 +1,13 @@
 package com.example.turtle.command;
 
-public class SE350Command {
+public class SE350Command extends CompositeCommand {
+    public SE350Command() {
+        add(new LetterSCommand())
+                .add(new PenUpCommand()).add(new MoveCommand(5)).add(new PenDownCommand())
+                .add(new LetterECommand())
+                .add(new PenUpCommand()).add(new MoveCommand(5)).add(new PenDownCommand())
+                .add(new DigitThreeCommand())
+                .add(new PenUpCommand()).add(new MoveCommand(5)).add(new PenDownCommand())
+                .add(new RectangleCommand(3,3));
+    }
 }
