451d3591ecb6ad6b66eba222dbb2189d1a3dcd18
ishqair shukair <ishukair@depaul.edu>
2025-06-08 19:43:43 -0500

    Create a rows×cols matrix and fill with spaces.

--- src/main/java/com.example.turtle/Matrix.java
+++ src/main/java/com.example.turtle/Matrix.java
@@ -7,4 +7,9 @@ public class Matrix {
     private final char[][] grid;
 
 
+    public Matrix(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        grid = new char[rows][cols];
+        clear();
 }
