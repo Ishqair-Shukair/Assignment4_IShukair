4907509fbfaa30b84219fd9aef7e6c42a549da59
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:24:33 -0500

    Move turtle without drawing

--- src/main/java/com.example.turtle/command/MoveCommand.java
+++ src/main/java/com.example.turtle/command/MoveCommand.java
@@ -1,4 +1,10 @@
 package com.example.turtle.command;
+import com.example.turtle.Turtle;
+
 
 public class MoveCommand {
+    private final double dist;
+    public MoveCommand(double d) { dist = d; }
+    @Override public void execute(Turtle t) { t.move(dist); }
+    @Override public void undo(Turtle t) { t.move(-dist); }
 }
