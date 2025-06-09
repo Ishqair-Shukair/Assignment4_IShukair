80fce971cf638800ac2b3126646275faac2a524b
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:11:49 -0500

    declared variables

--- src/main/java/com.example.turtle/Turtle.java
+++ src/main/java/com.example.turtle/Turtle.java
@@ -1,4 +1,14 @@
 package com.example.turtle;
 
 public class Turtle {
+
+    private double x = 0, y = 0;
+    private double heading = 0; // 0° = east, +° CCW
+    private final Pen pen;
+    private final Matrix canvas;
+
+    public Turtle(Matrix canvas) {
+        this.canvas = canvas;
+        this.pen = new Pen();
+    }
 }
