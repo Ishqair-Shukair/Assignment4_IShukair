bcbb2254be918cc1a3314ce839c3871400701bd7
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:50:52 -0500

    Draws a simple “E” shape of size 3×3.

--- src/main/java/com.example.turtle/command/DigitThreeCommand.java
+++ src/main/java/com.example.turtle/command/DigitThreeCommand.java
@@ -1,4 +1,8 @@
 package com.example.turtle.command;
 
-public class DigitThreeCommand {
+public class DigitThreeCommand extends CompositeCommand {
+    public DigitThreeCommand() {
+        add(new LetterECommand());
+        // flip horizontally: turn & redraw if desired
+    }
 }
