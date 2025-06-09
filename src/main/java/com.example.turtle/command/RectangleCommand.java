19b61c9df7306cc9e7d9a3c7235f3d41359d444e
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:47:26 -0500

    Draws a rectangle of given width/height.

--- src/main/java/com.example.turtle/command/RectangleCommand.java
+++ src/main/java/com.example.turtle/command/RectangleCommand.java
@@ -1,4 +1,15 @@
 package com.example.turtle.command;
 
-public class RectangleCommand {
+public class RectangleCommand extends CompositeCommand {
+    public RectangleCommand(double w, double h) {
+        // assume pen is already down before execution
+        add(new TraceCommand(w))
+                .add(new TurnCommand(90))
+                .add(new TraceCommand(h))
+                .add(new TurnCommand(90))
+                .add(new TraceCommand(w))
+                .add(new TurnCommand(90))
+                .add(new TraceCommand(h))
+                .add(new TurnCommand(90));
+    }
 }
