c1adbb7d24f7e77fb07aef68c7fcb0ce4b4cc187
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:50:15 -0500

    Draws a simple “E” shape of size 3×3.

--- src/main/java/com.example.turtle/command/LetterECommand.java
+++ src/main/java/com.example.turtle/command/LetterECommand.java
@@ -1,4 +1,18 @@
 package com.example.turtle.command;
 
-public class LetterECommand {
+public class LetterECommand extends CompositeCommand {
+    public LetterECommand() {
+        add(new TraceCommand(3))    // top bar
+                .add(new TurnCommand(90))
+                .add(new TraceCommand(3)) // spine down
+                .add(new TurnCommand(-90))
+                .add(new TraceCommand(2)) // middle bar
+                .add(new PenUpCommand())  // lift pen to move back
+                .add(new TurnCommand(180))
+                .add(new MoveCommand(2))
+                .add(new TurnCommand(-90))
+                .add(new PenDownCommand())
+                .add(new TraceCommand(3)) // bottom bar
+                .add(new TurnCommand(180)); // reset dir
+    }
 }
