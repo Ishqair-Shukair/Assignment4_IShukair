3cda40919c925be454d4226ca96bbf228edc47ff
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:43:55 -0500

    initiating

--- src/main/java/com.example.turtle/command/CompositeCommand.java
+++ src/main/java/com.example.turtle/command/CompositeCommand.java
@@ -1,4 +1,13 @@
 package com.example.turtle.command;
+import com.example.turtle.Turtle;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CompositeCommand {
+public class CompositeCommand implements Command {
+    protected final List<Command> children = new ArrayList<>();
+
+    public CompositeCommand add(Command c) {
+        children.add(c);
+        return this;
+    }
 }
