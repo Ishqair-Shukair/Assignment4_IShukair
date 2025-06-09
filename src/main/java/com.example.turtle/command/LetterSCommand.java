8f7b1a4ff8c209c46877e4b15aa9fb81b7ca09e8
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:48:48 -0500

    Draws a simple “S” shape of size 3×3.

--- src/main/java/com.example.turtle/command/LetterSCommand.java
+++ src/main/java/com.example.turtle/command/LetterSCommand.java
@@ -1,4 +1,16 @@
 package com.example.turtle.command;
 
-public class LetterSCommand {
+public class LetterSCommand extends CompositeCommand {
+    public LetterSCommand() {
+        add(new TraceCommand(3))
+                .add(new TurnCommand(90))
+                .add(new TraceCommand(1))
+                .add(new TurnCommand(90))
+                .add(new TraceCommand(3))
+                .add(new TurnCommand(-90))
+                .add(new TraceCommand(1))
+                .add(new TurnCommand(-90))
+                .add(new TraceCommand(3))
+                .add(new TurnCommand(180)); // face original dir
+    }
 }
