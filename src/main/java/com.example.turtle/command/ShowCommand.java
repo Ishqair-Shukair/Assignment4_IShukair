edd103ce4b295e74742f45eea9d872f10a0ac7b1
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:29:49 -0500

    Print the current canvas.

--- src/main/java/com.example.turtle/command/ShowCommand.java
+++ src/main/java/com.example.turtle/command/ShowCommand.java
@@ -1,4 +1,9 @@
 package com.example.turtle.command;
+import com.example.turtle.Turtle;
 
 public class ShowCommand {
+    @Override public void execute(Turtle t) {
+        System.out.print(t.show());
+    }
+    @Override public void undo(Turtle t) { /* no‐op */ }
 }
