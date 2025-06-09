0caff2b313b00c88b6ad6b5b34d05e85154737ee
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:10:31 -0500

    Draw from (x0,y0) to (x1,y1) if pen is down.

--- src/main/java/com.example.turtle/Pen.java
+++ src/main/java/com.example.turtle/Pen.java
@@ -12,4 +12,10 @@ public class Pen {
     public void setStrategy(DrawingStrategy s) {
         this.strategy = s;
     }
+
+    public void draw(Matrix m, double x0, double y0, double x1, double y1) {
+        if (down) strategy.drawLine(m, x0, y0, x1, y1);
+    }
+
+    public boolean isDown() { return down; }
 }
