a09a4236de07af8af20f1d493d4c7b723f5a2b8a
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:57:40 -0500

    StrategyTest

--- src/test/java/com.example.turtle/StrategyTest.java
+++ src/test/java/com.example.turtle/StrategyTest.java
@@ -1,4 +1,20 @@
 package com.example.turtle;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class StrategyTest {
+    @Test
+    public void testBresenham() {
+        Matrix m = new Matrix(5,5);
+        new BresenhamStrategy().drawLine(m, 0,0,4,4);
+        assertEquals('#', m.toString().split("\n")[4].charAt(4));
+    }
+
+    @Test
+    public void testNaive() {
+        Matrix m = new Matrix(5,5);
+        new NaiveStrategy().drawLine(m, 0,4,4,0);
+        assertEquals('#', m.toString().split("\n")[0].charAt(4));
+    }
 }
