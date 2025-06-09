2f0a1d929e78dcf48fb583abf0b647b8d8e92d89
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:26:50 -0500

    Rotate the turtle

--- src/main/java/com.example.turtle/command/TurnCommand.java
+++ src/main/java/com.example.turtle/command/TurnCommand.java
@@ -1,4 +1,10 @@
 package com.example.turtle.command;
+import com.example.turtle.Turtle;
+
 
 public class TurnCommand {
+    private final double angle;
+    public TurnCommand(double a) { angle = a; }
+    @Override public void execute(Turtle t) { t.turn(angle); }
+    @Override public void undo(Turtle t) { t.turn(-angle); }
 }
