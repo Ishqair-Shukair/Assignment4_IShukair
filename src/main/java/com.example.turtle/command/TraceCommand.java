a7fd26270d37357bbe3f5fc8ca227b68c9969a0a
ishqair shukair <ishukair@depaul.edu>
2025-06-08 20:25:57 -0500

    Move turtle with drawing

--- src/main/java/com.example.turtle/command/TraceCommand.java
+++ src/main/java/com.example.turtle/command/TraceCommand.java
@@ -1,4 +1,15 @@
 package com.example.turtle.command;
+import com.example.turtle.Turtle;
+
 
 public class TraceCommand {
+    private final double dist;
+    public TraceCommand(double d) { dist = d; }
+    @Override public void execute(Turtle t) { t.trace(dist); }
+    @Override public void undo(Turtle t) {
+        // To undo drawing, we simply move back without drawing.
+        t.getPen().up();
+        t.move(-dist);
+        t.getPen().down();
+    }
 }
